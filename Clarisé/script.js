4c0e129e28f4d200bbc9e29b3a316e7cc55b6831
ステゴデザイン <actiasmoooth@gmail.com>
2026-02-20 13:57:23 +0900

    新しいサイトの追加

---
+++ Clarisé/script.js
@@ -0,0 +1,8 @@
+// 置換案（jQuery を使わない）
+const Humburger = document.getElementById('js-hg');
+const Nav = document.getElementById('js-nav');
+
+Humburger.addEventListener('click', function() {
+  this.classList.toggle('open'); // ボタンの開閉アニメーション用
+  Nav.classList.toggle('open');  // ナビの表示切替
+});
